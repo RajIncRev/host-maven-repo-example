fdaff0d41f0175225301fe5c906633a20dc92b9b
RajIncRev <rajkishore.behura@gmail.com>
2022-06-20 13:04:53 +0530

    initial checkin

---
+++ src/main/java/com/increv/maven/HostMavenRepoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.increv.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostMavenRepoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostMavenRepoExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/increv/maven/HostMavenRepoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.increv.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostMavenRepoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
